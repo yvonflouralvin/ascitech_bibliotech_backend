ececd4d3e8ef98f729f24a910ad67e2fd17ceab4
Yvon Flour <me@yvonflour.com>
2025-12-29 16:36:36 +0100

    fixe database

--- backend/settings.py
+++ backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'bibliotech_db',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'prod_postgres',
