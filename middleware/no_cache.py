b63c3e64b3a8846f62a8baea7f97d423e72a9cb3
Yvon Flour Alvin <me@yvonflour.com>
2026-01-12 13:41:00 +0000

    forcer le reload

---
+++ middleware/no_cache.py
@@ -0,0 +1,12 @@
+# middleware/no_cache.py
+class NoCacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.path.startswith("/static/"):
+            response["Cache-Control"] = "no-cache, no-store, must-revalidate"
+            response["Pragma"] = "no-cache"
+            response["Expires"] = "0"
+        return response
